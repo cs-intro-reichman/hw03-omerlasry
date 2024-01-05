8cf55188efe83db5f22830c4684ea0e6c171b686
unknown <omer.lasry@post.runi.ac.il>
2024-01-05 22:47:37 +0200

    fixed program

--- UniqueChars.java
+++ UniqueChars.java
@@ -10,8 +10,24 @@ public class UniqueChars {
      * except that all the duplicate characters are removed,
      * unless they are space characters.
      */
-    public static String uniqueChars(String s) {
+    public static String uniqueChars(String s) 
+	{
         // Replace the following statement with your code
-        return null;
+		String ans = "" ;
+		for( int i = 0 ; i<s.length() ; i++)
+		{
+			char ch = s.charAt(i);
+			boolean b = true;
+			for( int j = i+1 ; j<s.length() ; j++)
+			{
+				if(ch == s.charAt(j)&&ch!= 32)
+				{
+					b = false;
+				}
+			}
+			if(b == true)
+				ans = ans + (char)s.charAt(i);
+		}
+        return ans;
     }
 }
